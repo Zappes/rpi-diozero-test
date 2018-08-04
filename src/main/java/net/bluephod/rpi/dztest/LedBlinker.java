9d49627f4a065e2223818869ab79d4f595553335
Gregor Ottmann <zaphod@bluephod.net>
2018-08-04 17:28:19 +0200

    Added stuff for the LED strip and copied the sample code so I could edit it easily.

--- src/main/java/net/bluephod/rpi/dztest/LedBlinker.java
+++ src/main/java/net/bluephod/rpi/dztest/LedBlinker.java
@@ -1,8 +1,21 @@
 package net.bluephod.rpi.dztest;
 
+import com.diozero.devices.LED;
+
 public class LedBlinker {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		System.out.println("LED Blinker demo");
+
+		try(LED led = new LED(18)) {
+			System.out.println("Setting pin 12 to HIGH - press return to go to low.");
+			led.on();
+			System.in.read();
+
+			System.out.println("Setting pin 12 to LOW and exiting");
+			led.off();
+		}
+
+
 		System.exit(0);
 	}
 }
